a944ee5b041422cbab6b68eece868f298cb3d7fc
Milton Lima <milton.mlima@gmail.com>
2021-04-11 09:27:43 -0300

    crud devedores e dívidas para Receiv

---
+++ index.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="pt">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link href="bootstrap-4.0.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
+  <title>Receiv</title>
+</head>
+
+<body>
+  Hello World
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="js/jquery-3.5.1.min.js"></script>
+  <script src="bootstrap-4.0.0/assets/js/vendor/popper.min.js"></script>
+  <script src="bootstrap-4.0.0/dist/js/bootstrap.min.js"></script>
+</body>
+
+</html>
