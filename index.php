b2984d5d758428be5231570e0b2e3e32270865ee
Milton Lima <milton.mlima@gmail.com>
2021-04-11 09:35:04 -0300

    pasta js e arquivos jquery-3.5.1.min.js e script.js criados

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
   <!-- Bootstrap CSS -->
-  <link href="bootstrap-4.0.0/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link href="bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
 
   <title>Receiv</title>
 </head>
@@ -17,8 +17,9 @@
   <!-- Optional JavaScript -->
   <!-- jQuery first, then Popper.js, then Bootstrap JS -->
   <script src="js/jquery-3.5.1.min.js"></script>
-  <script src="bootstrap-4.0.0/assets/js/vendor/popper.min.js"></script>
-  <script src="bootstrap-4.0.0/dist/js/bootstrap.min.js"></script>
+  <script src="bootstrap/assets/js/vendor/popper.min.js"></script>
+  <script src="bootstrap/dist/js/bootstrap.min.js"></script>
+  <script src="js/script.js"></script>
 </body>
 
 </html>
