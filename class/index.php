e71f74ec7b1f4091f2bddd269313655894e5cb16
Milton Lima <milton.mlima@gmail.com>
2021-04-11 09:40:41 -0300

    criação de construtor e método

--- class/index.php
+++ class/index.php
@@ -1,5 +1,23 @@
 <?php
 class devedor
 {
+  public $servername = 'localhost';
+  public $username = 'root';
+  public $password = '';
+  public $dbname = 'receiv';
 
+  function devedor()
+  {
+    $this->acessoBanco();
+  }
+
+  public function acessoBanco()
+  {
+    global $conn;
+    $conn = new mysqli($this->servername, $this->username, $this->password, $this->dbname);
+    $conn->set_charset('utf8');
+    if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+    }
+  }
 }
